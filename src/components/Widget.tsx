c51b1495778d7a2ebffc294040e6e5c30e34f4db
Kai Davenport <kaiyadavenport@gmail.com>
2024-04-03 13:16:05 +0100

    dont add api token

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,15 +1,10 @@
 import { FC, createElement, useState } from 'react'
-import { styled, setup } from 'goober'
+import { setup } from 'goober'
 
 import Window, { WindowTheme } from './Window'
 import Search, { SearchTheme } from './Search'
 
 setup(createElement)
-
-const SearchContainer = styled("div")`
-  background-color: #070714;
-  padding: 200px;
-`;
 
 const Widget: FC<{
   url: string,
@@ -30,28 +25,26 @@ const Widget: FC<{
   
   return (
     <>
-      <SearchContainer>
-        <Search
-          theme={ searchTheme }
-          placeholder={ placeholder }
-          onClick={ () => setOpen(true) }
-        />
-        {
-          open && (
-            <Window
-              url={ url }
-              model={ model }
-              bearerToken={ bearerToken }
-              modalTheme={ windowTheme }
-              searchBoxTheme={ searchTheme }
-              placeholder={ placeholder }
-              onClose={ () => {
-                setOpen(false)
-              }}
-            />
-          )
-        }
-      </SearchContainer>
+      <Search
+        theme={ searchTheme }
+        placeholder={ placeholder }
+        onClick={ () => setOpen(true) }
+      />
+      {
+        open && (
+          <Window
+            url={ url }
+            model={ model }
+            bearerToken={ bearerToken }
+            modalTheme={ windowTheme }
+            searchBoxTheme={ searchTheme }
+            placeholder={ placeholder }
+            onClose={ () => {
+              setOpen(false)
+            }}
+          />
+        )
+      }
     </>
   )
 }
