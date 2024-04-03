f67197399e1311b9c0855420fc0ccf7e56534ade
Kai Davenport <kaiyadavenport@gmail.com>
2024-04-03 19:14:35 +0100

    publish to npm

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,21 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: 'src/index.tsx',
+      name: 'helix-chat-widget',
+      formats: ['es'],
+      fileName: (format) => `index.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  }
 })
